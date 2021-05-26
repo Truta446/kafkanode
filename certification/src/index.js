654e2099645da8389409cd3d0076587580a3e832
Juan Versolato Lopes <juan.versolato@outlook.com>
2021-05-26 15:37:25 -0400

    First commit.

---
+++ certification/src/index.js
@@ -0,0 +1,43 @@
+import { Kafka, logLevel } from 'kafkajs';
+
+const kafka = new Kafka({
+  brokers: ['localhost:9092'],
+  clientId: 'centificate',
+  retry: {
+    initialRetryTime: 100,
+    retries: 3,
+  },
+  logLevel: logLevel.NOTHING,
+});
+
+const topic = 'issue-certificate';
+const consumer = kafka.consumer({ groupId: 'certificate-group' });
+
+const producer = kafka.producer();
+
+async function run() {
+  await producer.connect();
+  await consumer.connect();
+
+  await consumer.subscribe({ topic });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      const prefix = `${topic}[${partition} | ${message.offset}] / ${message.timestamp}`;
+      console.log(`- ${prefix} ${message.key}#${message.value}`);
+
+      const payload = JSON.parse(message.value);
+
+      setTimeout(() => {
+        producer.send({
+          topic: 'certification-response',
+          messages: [
+            { value: `Certificado do usuário ${payload.user.name} do curso ${payload.course} gerado com sucesso!` }
+          ]
+        })
+      }, 3000);
+    }
+  });
+}
+
+run().catch(console.error);
